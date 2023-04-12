83794ceaf76719111e406a266ef41c7e8d2eaf36
Yasmin haidar <yasmin1635@hotmail.com>
2023-04-12 09:52:10 +0100

    Flight class completed

--- src/main/java/Flight.java
+++ src/main/java/Flight.java
@@ -1,2 +1,37 @@
+import java.util.ArrayList;
+
 public class Flight {
+
+    private String destination;
+    private String id;
+
+    private ArrayList<Passenger> passengers;
+
+    public Flight(String destination, String id){
+        this.destination = destination;
+        this.id = id;
+        this.passengers = new ArrayList<>();
+
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+
+    public void addPassenger(Passenger passenger){
+        this.passengers.add(passenger);
+    }
+
+    public int getPassengerCount(){
+        return this.passengers.size();
+    }
+
+
+
+
 }
